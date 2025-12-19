bed69bc9893f4e9be73392af570dbcdcf9371c4b
MowlandCodes <mowlandgaming@gmail.com>
2025-12-19 21:31:09 +0700

    update to use C++ instead of C

--- controller/main/main.cpp
+++ controller/main/main.cpp
@@ -1,9 +1,8 @@
 #include "esp_log.h"
-#include "sdkconfig.h"
 
 static const char *TAG = "MAIN";
 
-void app_main(void)
+extern "C" void app_main(void)
 {
     ESP_LOGI(TAG, "Hello from app_main");
     ESP_LOGI(TAG, "Hello from app_main2");
